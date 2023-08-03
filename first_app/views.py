74e8e330407d2300349e5042b4f770c50a52f43a
pro <pro@prodeMacBook-Pro-2.local>
2023-08-03 13:37:16 +0600

    starting project

---
+++ first_app/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, './first_app/index.html', {'name': 'Tanver Rana Sobur', 'age': 19, 'marks': 90, 'courses': [
+        {
+            'id': 1,
+            'course': 'C',
+            'teacher': 'Arifin'
+        },
+        {
+            'id': 2,
+            'course': 'C++',
+            'teacher': 'Rokey'
+        },
+        {
+            'id': 3,
+            'course': 'Database',
+            'teacher': 'Iftekhar'
+        },
+    ], 'name': 'Tanver', 'lst': [1, 2, 4, 7], 'blog': 'I am Tanver Rana Sobur. I am the student of Bangladesh University department of CSE. THis is our blog website.'})
